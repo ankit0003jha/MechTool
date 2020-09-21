7dcf34cc45d1a6085c7df9905e69a5ac45307e8b
Ankit jha <65860426+ankit0003jha@users.noreply.github.com>
2020-09-22 03:11:06 +0530

    Update urls.py

--- homes/urls.py
+++ homes/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('contacts', views.contacts, name='contacts'),
     path('login', views.login, name="login"),
