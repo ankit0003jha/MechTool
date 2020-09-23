a0dbb5d7cb50a8dff6df4f36d864ab7c1bfe0791
ankit0003jha <65860426+ankit0003jha@users.noreply.github.com>
2020-09-23 11:41:41 +0530

    initial

--- kiTaNs/settings.py
+++ kiTaNs/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -140,3 +141,5 @@ MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+# Activate Django-Heroku.
+django_heroku.settings(locals())
